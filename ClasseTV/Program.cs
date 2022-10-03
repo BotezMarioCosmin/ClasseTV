702b2498f5b76d8e6874ac19b1c92857d4c175d7
Botez Mario Cosmin <botezmariocosmin@gmail.com>
2022-10-03 22:52:23 +0200

    Add project files.

---
+++ ClasseTV/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseTV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        class TV
+        {
+            bool stato; //accesa o spenta
+
+            float potenza; //watt
+
+            int luminosita;
+
+            string produttore; //casa produttrice
+
+            string modello;
+
+            int quantitaPixelAltezza; //es 1080p
+
+            int quantitaPixelLunghezza; //es 1920p
+
+            int pollici; //es 55
+
+            string tipologiaPannello; //es led, oled...
+
+            int canaleSelezionato; //42
+
+            bool ethernet; //presenza di porta ethernet
+
+            bool WIFI; //presenza di scheda di rete wireless
+
+            bool statoRete; //connesso o non connesso
+
+            int quantitaPorteHDMI; //es 4
+
+
+        }
+    }
+}
